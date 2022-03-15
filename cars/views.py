aa99ecde41636fa133d5d831e7c9cdd8437c691a
fred <fredwright95@yahoo.com>
2022-03-14 20:35:00 -0400

    registered car model with admin site + completed post function for car

--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,21 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Car
+from .serializers import CarSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST',])
 def cars_list(request):
-
-
-    return Response('ok')
-
+    
+    
+   if request.method == 'GET': 
+     cars = Car.objects.all()
+     serializer = CarSerializer(cars, many=True)
+     return Response(serializer.data)
+   elif request.method ==  'POST':
+       serializer = CarSerializer(data=request.data)
+       serializer.is_valid(raise_exception=True)
+       serializer.save()
+       return Response(serializer.data, status=status.HTTP_201_CREATED)
+      
+           
